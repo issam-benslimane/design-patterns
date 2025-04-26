2e116d2a142d477bc18d20a3f6f488051fd26443
issam <issambenslimane9999@hotmail.com>
2025-04-26 15:43:01 +0100

    Revert "assignement"
    This reverts commit f87d14c56b67da9be7669dcdb55bd4c8b71a41ad.

--- src/exercice1/PluginFactory.java
+++
@@ -1,5 +0,0 @@
-public class PluginFactory {
-    public static Plugin createPlugin(String className) throws Exception {
-        return (Plugin) Class.forName(className).newInstance();
-    }
-} 
--- src/exercice1/PluginImpl.java
+++
@@ -1,9 +0,0 @@
-public abstract class PluginImpl implements Plugin {
-    public final void operation() {
-        partie1();
-        partie2();
-    }
-
-    protected abstract void partie1();
-    protected abstract void partie2();
-} 
